960c440b3d4cb9b47915979fb8e6828014316b3b
leaztanda <leaztanda@gmail.com>
2018-05-05 23:00:51 +0200

    add scrollview + image width auto in chapter

--- src/components/Chapter/Chapter.js
+++ src/components/Chapter/Chapter.js
@@ -1,19 +1,28 @@
 import React from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native'
+import { StyleSheet, View, ScrollView, Text, Button, Image, Dimensions } from 'react-native'
 import {connect} from 'react-redux';
+import resolveAssetSource from 'resolveAssetSource';
+import image from './../../assets/Chap27_part1.png';
 
 class Chapter extends React.Component {
 
   render () {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Chapter</Text>
-        <Button title={'Retour'} onPress={() => this.props.navigation.goBack()}/>
+      <View style={styles.container} >
+        <ScrollView horizontal={true}>
+          {/* <View style={styles.view} > */}
+            <Image source={require('./../../assets/Chap27_part1.png')} style={styles.image}/>
+          {/* </View> */}
+        </ScrollView>
       </View>
     )
   }
 
 }
+
+const source = resolveAssetSource(image);
+const height = Dimensions.get('window').height
+const ratio = height / source.height
 
 const styles = StyleSheet.create({
   container: {
@@ -24,6 +33,12 @@ const styles = StyleSheet.create({
     fontSize: 36,
     fontWeight: 'bold',
     color: 'white'
+  },
+  image: {
+    flex: 1,
+    height: height,
+    width: ratio * source.width,
+    resizeMode: 'cover'
   }
 })
 
